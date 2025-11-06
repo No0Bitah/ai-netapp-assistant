d3da9a2e0ec571bb94c41e4fea37e6263f876cda
No0Bitah <jomarydaison@gmail.com>
2025-11-06 08:50:47 +0800

    refactor: remove unused backend files and update frontend structure
    - Deleted alembic.ini, models.py, migration scripts, and README from backend.
    - Updated Dockerfile to install dependencies and expose Vite port.
    - Modified index.html to include favicon and updated script source.
    - Enhanced package.json with new dependencies and linting scripts.
    - Replaced App.tsx with App.jsx and updated main.tsx to main.jsx.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+        <h1>This ismy first page react</h1>
+    </>
+  )
+}
+
+export default App
